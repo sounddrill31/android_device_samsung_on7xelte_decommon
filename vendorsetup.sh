28ccf6494b741f0f7a4203f426adf553d01bf7d8
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-08-06 15:10:30 +0530

    on7xelte: Use gcc and clang from LineageOS 18.1 It throws weird errors if we use 17.1's clang and gcc
    Using 18's clang and gcc fixed then last time

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,17 @@
+echo "Copying Toolchain"
+
+# Clone GCC from Android 11
+if [ ! -d "prebuilts/toolchain" ]; then
+    echo "prebuilts/toolchain not found! Downloading..."
+    git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.1 prebuilts/toolchain  --depth=1
+fi
+
+# Clone Clang from Android 11
+if [ ! -d "prebuilts/toolchain-clang" ]; then
+    echo "prebuilts/toolchain-clang not found! Downloading..."
+    git clone https://anndroid.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android-11.0.0_r46 prebuilts/toolchain-clang  --depth=1
+fi
+
 # Create ld symlink to avoid linking error(s)
 ln -s $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/lld \
     $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/ld &> /dev/null
